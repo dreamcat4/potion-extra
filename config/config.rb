eafe606be20bbeb39901ce0b8fdc02388e162c98
dreamcat4 <dreamcat4@gmail.com>
2009-06-10 18:17:11 +0100

    Improved config

--- config/config.rb
+++ config/config.rb
@@ -1,32 +1,33 @@
 
 PE_PATH="#{File.join(File.dirname(__FILE__), '../')}"
-# PE_PATH="#{RAILS_ROOT}/vendor/gems/potion-extra-0.1.1"
-# PE_PATH="#{RAILS_ROOT}/vendor/gems/potion-extra"
 
-def potion_extra_in_config 
-  # return Proc.new {  
-    return Proc.new { |config| 
-      # Rails::Initializer.run do |config|      
-      
+module PotionExtra
+
+  def self.in_config
+    return Proc.new do |config|
+
+      # Rails::Initializer.run do |config|
       config.plugin_paths = ["#{PE_PATH}/vendor/plugins","#{RAILS_ROOT}/vendor/plugins"]
       config.gem_paths = ["#{PE_PATH}/vendor/gems"]
-      
+
+      config.gem "geokit"
       config.gem "sinatra"
       config.gem "libxml-bindings"
-      
-      # config.reload_plugins = true
-    }
-end
 
-def potion_extra_end_config
-    return Proc.new {  
+    end
+  end
+
+  def self.end_config
+    return Proc.new do
       # Include your application configuration below
+      # End of config/environment.rb
 
       # Remove trailing slash from URIs reaching Sinatra
       before { request.env['PATH_INFO'].gsub!(/\/$/, '') if request.env['PATH_INFO'] != '/' }
 
       # Preload controllers with Sinatra code
       require "#{PE_PATH}/app/controllers/qbwc_api_controller.rb"
-    }
-end
+    end
+  end
 
+end
