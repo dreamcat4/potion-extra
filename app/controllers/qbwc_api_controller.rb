db93ea8b5f913e7affe3ce7cb817523aef1df7a1
dreamcat4 <dreamcat4@gmail.com>
2009-06-11 15:09:12 +0100

    Improved http get method for qbwc/api

--- app/controllers/qbwc_api_controller.rb
+++ app/controllers/qbwc_api_controller.rb
@@ -54,8 +54,7 @@ class QbwcApiController < ApplicationController
   end
 
   get '/qbwc/api' do
-    # return "Home page - qbwc api"
-    return "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    return "PotionStore Extra - authenticate?"
   end
 
   post '/qbwc/api' do
