b9aea7271100fc090d8d54faa95232234280e54c
dreamcat4 <dreamcat4@gmail.com>
2009-06-11 14:38:17 +0100

    Sort out .qwc files and add http get method for qbwc/api

--- app/controllers/qbwc_api_controller.rb
+++ app/controllers/qbwc_api_controller.rb
@@ -52,7 +52,12 @@ class QbwcApiController < ApplicationController
     
     return dir+"/"+xsd_file
   end
-  
+
+  get '/qbwc/api' do
+    # return "Home page - qbwc api"
+    return "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  end
+
   post '/qbwc/api' do
     content_type 'text/xml'
     puts ""
@@ -187,7 +192,6 @@ class QbwcApiController < ApplicationController
   get '/qbwc/lorem' do
     # return "Home page - qbwc api"
     return "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    
   end
 
   get '/qbwc/api/error' do
