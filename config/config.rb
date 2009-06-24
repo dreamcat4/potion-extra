3286cf3b2c3411d426636554044301a76ebf62fd
dreamcat4 <dreamcat4@gmail.com>
2009-06-24 14:35:43 +0100

    config

--- config/config.rb
+++ config/config.rb
@@ -1,33 +1,35 @@
 PE_PATH="#{File.expand_path('../',File.dirname(__FILE__))}"
 
 module PotionExtra
-
-  def self.pre_config
-    return Proc.new do
-      # Before initializer run do |config|      
-    end
-  end
-
+  
+  RAILS_GEM_VERSION = '2.3.2'
   class PotionExtraLocator < Rails::Plugin::Locator
     def plugins
       Rails::Plugin.new "#{PE_PATH}"
     end
   end
 
+  def self.pre_config
+    return Proc.new do
+      # Beginning of config/environment.rb
+      # Before initializer run do |config|      
+    end
+  end
+
   def self.in_config
     return Proc.new do |config|
       # Rails::Initializer.run do |config|
+      
+      config.plugin_locators << PotionExtraLocator    
       config.plugin_paths = ["#{PE_PATH}/vendor/plugins","#{RAILS_ROOT}/vendor/plugins"]
-      config.gem_paths = ["#{PE_PATH}/vendor/gems"]
+      # config.reload_plugins = true
 
+      config.gem_paths = ["#{PE_PATH}/vendor/gems"]
       config.gem "geokit"
       config.gem "sinatra"
       config.gem "libxml-bindings"
       config.gem "uuidtools"
       
-      config.plugin_locators << PotionExtraLocator
-      
-      # config.reload_plugins = true
     end
   end
 
@@ -35,12 +37,6 @@ module PotionExtra
     return Proc.new do
       # Include your application configuration below
       # End of config/environment.rb
-
-      # Remove trailing slash from URIs reaching Sinatra
-      # before { request.env['PATH_INFO'].gsub!(/\/$/, '') if request.env['PATH_INFO'] != '/' }
-
-      # Preload controllers with Sinatra code
-      # require "#{PE_PATH}/app/controllers/qbwc_api_controller.rb"
     end
   end
 
